327d92741916b9e2f8e7dbc897540530dbe05a7b
Talwinderr <61751175+Talwinderr@users.noreply.github.com>
2026-02-10 19:34:35 +0000

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 export default {
-	// Doesn't seem to work with Framer Motion in React 19 atm:
-	// https://github.com/framer/motion/issues/2668
-	reactStrictMode: false,
-	experimental: {
-		reactCompiler: {
-			compilationMode: 'all',
-			panicThreshold: 'CRITICAL_ERRORS'
-		}
-	}
-}
+  // Doesn't seem to work with Framer Motion in React 19 atm:
+  // https://github.com/framer/motion/issues/2668
+  reactStrictMode: false,
+  experimental: {
+    reactCompiler: {
+      compilationMode: 'all',
+      panicThreshold: 'CRITICAL_ERRORS'
+    }
+  },
+  images: {
+    unoptimized: true,
+  },
+};
