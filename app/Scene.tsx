f6f326a59dedd46c638fa7a437366ac69ecb7b82
Talwinderr <tally.basra@gmail.com>
2026-02-11 20:17:04 +0000

    Point code to statue.glb

--- app/Scene.tsx
+++ app/Scene.tsx
@@ -158,7 +158,7 @@ type GLTFResult = GLTF & {
 }
 
 function Venus(props: ThreeElements['group']) {
-	const { nodes, materials } = useGLTF('/statue_v2.glb') as GLTFResult
+	const { nodes, materials } = useGLTF('/statue.glb') as GLTFResult
 
 	applyProps(materials['Scene_-_Root'], {
 		color: '#030303',
@@ -180,7 +180,7 @@ function Venus(props: ThreeElements['group']) {
 	)
 }
 
-useGLTF.preload('/statue_v2.glb')
+useGLTF.preload('/statue.glb')
 
 type RadialGradientTextureProps = Omit<ThreeElements['canvasTexture'], 'args'> & {
 	stops: Array<number>
